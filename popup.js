d93e07fff0f4391fdb82fe5e570db4e281b50a24
piesieomane <piesieyeboah@gmail.com>
2022-06-14 21:40:30 +0300

    fix lint errors

--- popup.js
+++ popup.js
@@ -11,7 +11,7 @@ const projects = [
 
   {
     name: 'Multi-Post Stories',
-    img: 'images/Snapshoot-Portfolio-resize.png',
+    img: 'images/Snapshoot-Portfolio-resize1.png',
     description:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     technologies: ['html', 'css', 'javascript'],
